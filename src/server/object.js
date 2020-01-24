04ddbb631e51ee08f6f1c52ba2e9fbecffab9849
bong12345yt <thienbongyt@gmail.com>
2020-01-24 15:59:53 +0700

    Server: Create object class

---
+++ src/server/object.js
@@ -0,0 +1,11 @@
+class Object {
+  constructor(id, x, y, dir, speed) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.direction = dir;
+    this.speed = speed;
+  }
+}
+
+module.exports = Object;
