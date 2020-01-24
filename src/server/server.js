cb221ed6e6c07c44be3ffb32e648d7f0be2c1d2b
bong12345yt <thienbongyt@gmail.com>
2020-01-24 20:18:10 +0700

    Setup webpack-dev and create index.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
-const socketio = require('socket.io');
 
 // Setup an Express server
 const app = express();
 app.use(express.static('public'));
+
+// Setup Webpack for development
+const config = require('../../webpack.dev.config.js');
+const compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler));
 
 // Listen on port
 const port = process.env.PORT || 3000;
@@ -13,15 +17,9 @@ const server = app.listen(port);
 console.log(`Server listening on port ${port}`);
 
 // Setup socket.io
-const io = socketio(server);
+const io = require('socket.io')(server);
 
 // Listen for socket.io connections
 io.on('connection', socket => {
   console.log('Player connected!', socket.id);
-
-  socket.on(Constants.MSG_TYPES.JOIN_GAME, joinGame);
-  socket.on(Constants.MSG_TYPES.INPUT, handleInput);
-  socket.on(Constants.MSG_TYPES.MOUSEPOS, handleMousePos);
-  socket.on(Constants.MSG_TYPES.INPUT_MOUSE_LEFT_CLICK, handleMouseLeftClick);
-  socket.on('disconnect', onDisconnect);
 });
