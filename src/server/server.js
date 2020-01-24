57b3d5651ad4664ccecc36ae40bbef92e2de8116
duongban <duongduban11@gmail.com>
2020-01-24 15:24:24 +0700

    Listen socket io connection

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
 const socketio = require('socket.io');
 
 // Setup an Express server
@@ -10,3 +11,17 @@ app.use(express.static('public'));
 const port = process.env.PORT || 3000;
 const server = app.listen(port);
 console.log(`Server listening on port ${port}`);
+
+// Setup socket.io
+const io = socketio(server);
+
+// Listen for socket.io connections
+io.on('connection', socket => {
+  console.log('Player connected!', socket.id);
+
+  socket.on(Constants.MSG_TYPES.JOIN_GAME, joinGame);
+  socket.on(Constants.MSG_TYPES.INPUT, handleInput);
+  socket.on(Constants.MSG_TYPES.MOUSEPOS, handleMousePos);
+  socket.on(Constants.MSG_TYPES.INPUT_MOUSE_LEFT_CLICK, handleMouseLeftClick);
+  socket.on('disconnect', onDisconnect);
+});
