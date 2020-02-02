a3ec95e99db22ae1c267d96bb6c19f110fdda76e
bong12345yt <thienbongyt@gmail.com>
2020-02-02 22:37:16 +0700

    Add firing bullet

--- src/server/game.js
+++ src/server/game.js
@@ -5,6 +5,7 @@ class Game {
   constructor() {
     this.sockets = {};
     this.players = {};
+    this.bullets = [];
     this.lastUpdateTime = Date.now();
     setInterval(this.update.bind(this), 1000 / 60);
   }
@@ -24,7 +25,9 @@ class Game {
   }
 
   handleInput(socket, dir) {
-    this.players[socket.id].setDirection(dir);
+    if (this.players[socket.id]) {
+      this.players[socket.id].setDirection(dir);
+    }
   }
 
   update() {
@@ -33,11 +36,24 @@ class Game {
     const dt = (now - this.lastUpdateTime) / 1000;
     this.lastUpdateTime = now;
 
+    // Update each bullet
+    const bulletsToRemove = [];
+    this.bullets.forEach(bullet => {
+      if (bullet.update(dt)) {
+        // Destroy this bullet
+        bulletsToRemove.push(bullet);
+      }
+    });
+    this.bullets = this.bullets.filter(bullet => !bulletsToRemove.includes(bullet));
+
     // Update each player
     Object.keys(this.sockets).forEach(playerID => {
       const socket = this.sockets[playerID];
       const player = this.players[playerID];
-      player.update(dt);
+      const newBullet = player.update(dt);
+      if (newBullet) {
+        this.bullets.push(newBullet);
+      }
       socket.emit(Constants.MSG_TYPES.GAME_UPDATE, this.createUpdate(player));
     });
   }
--- src/server/object.js
+++ src/server/object.js
@@ -1,10 +1,32 @@
 class Object {
-  constructor(id, x, y, dir, speed) {
-    this.id = id;
+  constructor(x, y, dir, speed) {
     this.x = x;
     this.y = y;
     this.direction = dir;
     this.speed = speed;
+  }
+
+  update(dt) {
+    this.x += dt * this.speed * Math.sin(this.direction);
+    this.y -= dt * this.speed * Math.cos(this.direction);
+  }
+
+  distanceTo(object) {
+    const dx = this.x - object.x;
+    const dy = this.y - object.y;
+    return Math.sqrt(dx * dx + dy * dy);
+  }
+
+  setDirection(dir) {
+    this.direction = dir;
+  }
+
+  serializeForUpdate() {
+    return {
+      x: this.x,
+      y: this.y,
+      direction: this.direction,
+    };
   }
 }
 
