fb4b64405f85c3f631b53c1144261b9d25419948
duongduban11@gmail.com <thienbongyt@gmail.com>
2020-02-13 21:22:31 +0700

    Update asset, load asset

--- src/client/assets.js
+++ src/client/assets.js
@@ -1,9 +1,28 @@
 const ASSET_NAMES = [
-  'ship.svg',
-  'bullet.svg',
+  'spritesheet.png',
+  'spritesheet2.png',
+  'spritesheet3.png',
+  'spritesheet4.png',
+  'spritesheet5.png',
+  'bullet.png',
+  'heath.png',
+  'HP_Bonus.png',
+  'Damage_Bonus.png',
+  'spritesheetspark.png',
+  'spritesheetexplosion.png',
+  'SpaceBackground-12.jpg',
+  'spritesheet_planet_01.png',
+  'spritesheet_star_blue.png',
+  'spritesheet_star_ograne.png',
+  'spritesheet_star_red.png',
+  'spritesheet_star_yellow.png',
+  'spritesheet_star_white.png',
+  'spritesheet_bomb_1_idle.png',
+  'spritesheet_bomb_1_explosion.png',
 ];
 
 const assets = {};
+const json = {};
 
 const downloadPromise = Promise.all(ASSET_NAMES.map(downloadAsset));
 
@@ -19,6 +38,22 @@ function downloadAsset(assetName) {
   });
 }
 
+export function readJSOn(jsonName) {
+  return new Promise(resolve => {
+    fetch(`/assets/${jsonName}`)
+      .then(response => response.json())
+      .then(data => { // Do stuff with the response
+        json[jsonName] = data;
+        resolve();
+      })
+      .catch(function (error) {
+        console.log('Looks like there was a problem: \n', error);
+      });
+  });
+}
+
 export const downloadAssets = () => downloadPromise;
 
 export const getAsset = assetName => assets[assetName];
+
+export const getJson = jsonName => json[jsonName];
--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import { connect, play } from './networking';
 import { startRendering, stopRendering } from './render';
 import { startCapturingInput, stopCapturingInput } from './input';
-import { downloadAssets } from './assets';
+import { downloadAssets, readJSOn } from './assets';
 import { initState } from './state';
 import { setLeaderboardHidden } from './leaderboard';
 
@@ -11,16 +11,28 @@ import './css/main.css';
 const playMenu = document.getElementById('play-menu');
 const playButton = document.getElementById('play-button');
 const usernameInput = document.getElementById('username-input');
+const shiptype = document.getElementById('shiptype').getElementsByClassName('active');
 
 Promise.all([
   connect(onGameOver),
   downloadAssets(),
+  readJSOn('spritesheet.json'),
+  readJSOn('spritesheetspark.json'),
+  readJSOn('spritesheetexplosion.json'),
+  readJSOn('spritesheet_star_blue.json'),
+  readJSOn('spritesheet_planet_01.json'),
+  readJSOn('spritesheet_star_ograne.json'),
+  readJSOn('spritesheet_star_red.json'),
+  readJSOn('spritesheet_star_yellow.json'),
+  readJSOn('spritesheet_star_white.json'),
+  readJSOn('spritesheet_bomb_1_explosion.json'),
+  readJSOn('spritesheet_bomb_1_idle.json'),
 ]).then(() => {
   playMenu.classList.remove('hidden');
   usernameInput.focus();
   playButton.onclick = () => {
     // Play!
-    play(usernameInput.value);
+    play(usernameInput.value,shiptype[0].getAttribute('value'));
     playMenu.classList.add('hidden');
     initState();
     startCapturingInput();
@@ -35,3 +47,4 @@ function onGameOver() {
   playMenu.classList.remove('hidden');
   setLeaderboardHidden(true);
 }
+
